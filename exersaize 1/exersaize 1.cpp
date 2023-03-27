4cfd98e5afaed105f973b0a81c9aaa76b7c7ccd6
iqwa1107 <ramahs49@gmail.com>
2023-03-27 10:16:10 +0700

    membuat prosedur

--- exersaize 1/exersaize 1.cpp
+++ exersaize 1/exersaize 1.cpp
@@ -5,3 +5,21 @@ double rerata(double a, double b) {
 	return (a + b) / 2;
 }
 
+string status(double c) {
+	if(c >= 70)
+		return"Lulus";
+	else
+		return "gagal";
+}
+
+string status2(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "lulus";
+	else 
+		return "gagal";
+}
+
+int main() {
+	double nilM, nilB
+		count 
+}
